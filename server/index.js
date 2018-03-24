6deea9633544bb3332e43384c9af7927234dfff2
Venu D <venu.dasarathy@gmail.com>
2018-03-24 17:22:55 +0530

    add knex, pg dependencies;setup env mode

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const app = express();
 const port = 4000;
 
 app.listen(port);
-console.log('Listening on port', port);
+console.log('Started server in mode: ',process.env.NODE_ENV,'. Listening on port', port);
